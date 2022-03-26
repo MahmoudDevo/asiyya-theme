be3f2b22566f1eed54695cc1c78c3279e00a223f
Mahmoud devo <contact@devosystem.com>
2022-03-26 22:39:48 +0100

    Initial commit

---
+++ config/client.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'html' => [
+		'common' => [
+			'template' => [
+				'baseurl' => public_path( 'vendor/shop/themes/asiyya-theme' ),
+			],
+		],
+		'themes' => [
+			'asiyya-theme' => 'asiyya-theme',
+		],
+	],
+	'jsonapi' => [
+	],
+];
