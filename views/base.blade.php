5a1869e08a31e6cfcd0c81847e3bf4a1b0299c05
Mahmoud devo <contact@devosystem.com>
2022-03-27 02:03:39 +0000

    DEVO-asiyya 2020

--- views/base.blade.php
+++ views/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-DEVO-asiyya
+DEVO-asiyya 2022
 <html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ in_array(app()->getLocale(), ['ar', 'az', 'dv', 'fa', 'he', 'ku', 'ur']) ? 'rtl' : 'ltr' }}">
 	<head>
 		<meta charset="utf-8">
